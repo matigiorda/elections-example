6f9947868443a871a48554e4ac86156c3909c8e5
matigiorda <matiasgiorda27@gmail.com>
2022-09-08 17:44:50 -0300

    Initial commit

---
+++ src/test/java/matias/giorda/electionsexample/ElectionsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package matias.giorda.electionsexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectionsExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
